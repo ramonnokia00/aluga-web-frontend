9f86d6884b53b76446bb085f06146adcb843cdf5
Macielsbdev <macielbezerra2018@gmail.com>
2025-06-09 23:01:38 -0300

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,7 @@ import Footer from "./components/Footer";
 function App() {
   return (
   <>
-  <Home />
-  <Header />
-  <SearchBar />
+  
   <Footer />
   </>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import icone from "../assets/icone-footer.png"
 
 export default function Footer() {
   return (
-    <footer className="bg-primary flex justify-between items-center px-6 py-3 text-white text-sm">
-      <span className="font-bold">🏠 Aluga Web</span>
-      <span>Todos os direitos reservados.</span>
+    <footer className="bg-[#DF4300] flex justify-between p-4 items-center">
+      <div className="flex gap-3">
+      <div className="bg-white rounded justify-center flex">
+        <img src={icone} alt="" className="p-1" />
+
+      </div>
+      <p className="text-white text-2xl font-bold">Aluga web</p>
+
+      </div>
+      <p className="text-white font-bold text-xl">Todos os direitos reservados</p>
+
     </footer>
   );
 }
