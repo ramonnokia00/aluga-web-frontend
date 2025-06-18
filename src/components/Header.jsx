8678023ee7d3982e67aed112ae700f921a1b8d97
ramonnokia00 <ramonsoares491@gmail.com>
2025-06-18 02:56:56 -0300

    oi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,17 +16,17 @@ export default function Header() {
     let overlayElement = document.querySelector('#overlay');
     let gavetaElement = document.querySelector("#gaveta");
 
-    if (overlayElement.classList.contains('invisible')){
-        overlayElement.classList.remove("invisible", "opacity-0");
-        gavetaElement.classList.remove("-right-full");
-        gavetaElement.classList.add("right-0");
+    if (overlayElement.classList.contains('invisible')) {
+      overlayElement.classList.remove("invisible", "opacity-0");
+      gavetaElement.classList.remove("-right-full");
+      gavetaElement.classList.add("right-0");
     }
     else {
-        overlayElement.classList.add('invisible' , 'opacity-0');
-        gavetaElement.classList.add("-right-full");
-        gavetaElement.classList.remove("right-0");
+      overlayElement.classList.add('invisible', 'opacity-0');
+      gavetaElement.classList.add("-right-full");
+      gavetaElement.classList.remove("right-0");
     }
-}
+  }
 
 
 
@@ -80,21 +80,21 @@ export default function Header() {
             alt="Foto"
             className="w-8 h-8 rounded-full object-cover border"
           />
-
+          <img
             src={usuario.foto || usuario.foto_url || "/user-default.png"}
             alt="Foto"
             className="w-8 h-8 rounded-full object-cover border cursor-pointer"
             onClick={mostrarOverlay}
           />
-           <div onclick="mostrarOverlay()" id="overlay"
-                class="w-full h-full bg-black/70 absolute top-0 left-0 duration-200 opacity-0 invisible">
-            </div>
-            <div id="gaveta" className="min-w-[350px] h-screen bg-white p-4 absolute top-0 -right-full duration-200 invisible">
-                <h3 onclick="mostrarOverlay()" className="flex justify-between items-center text-2xl ">Cadastro
-                    <img src={x} alt=""  className="w-[40px] h-[40px] bg-laranja fill-white p-2 cursor-pointer rounded-full hover:bg-pink-700 duration-200"
-                    onClick={mostrarOverlay}/>
-                </h3>
-                </div>
+          <div onclick="mostrarOverlay()" id="overlay"
+            class="w-full h-full bg-black/70 absolute top-0 left-0 duration-200 opacity-0 invisible">
+          </div>
+          <div id="gaveta" className="min-w-[350px] h-screen bg-white p-4 absolute top-0 -right-full duration-200 invisible">
+            <h3 onclick="mostrarOverlay()" className="flex justify-between items-center text-2xl ">Cadastro
+              <img src={x} alt="" className="w-[40px] h-[40px] bg-laranja fill-white p-2 cursor-pointer rounded-full hover:bg-pink-700 duration-200"
+                onClick={mostrarOverlay} />
+            </h3>
+          </div>
         </div>
       )}
     </header>
